4176d560fbbe0ba5fbe4836df0d010a91bb00983
alecmarkmorris <Alecmarkmorris@gmail.com>
2023-08-05 12:22:41 -0700

    Updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const POWERBALLAPI_URL = 'https://dark-rose-betta-ring.cyclic.app/';
 
-fetch(POWERBALLAPI_URL,{
-    mode:'no-cors'
-}).then(response => response.json())
+fetch(POWERBALLAPI_URL).then(response => response.json())
     .then(responseJson => console.log(responseJson)).catch(error => {
         console.log(error)
     })
