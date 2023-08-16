01555613c1afb4a2e707be2ddca151eb96ca2c91
alecmarkmorris <Alecmarkmorris@gmail.com>
2023-08-16 13:27:15 -0700

    Creating a single 3d array that contains smaller arrays of drawing numbers and dates

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const POWERBALLAPI_URL = 'https://dark-rose-betta-ring.cyclic.app/all';
+const POWERBALLAPI_URL = 'https://dark-rose-betta-ring.cyclic.app/2023';
 
+var drawings = [];
+var dates =[];
+var i =0;
+    fetch(POWERBALLAPI_URL)
+        .then(response => response.json())
+        .then(responseJson => {
+            for(let fact of responseJson ){
+                i++;
+                dates.push(fact)
 
-fetch(POWERBALLAPI_URL).then(response => response.json())
-    .then(responseJson => console.log(responseJson)).catch(error => {
-        console.log(error)
-    }); 
-    
+            if(i == 7){
+             //console.log(dates)
+             drawings.push(dates)
+             dates = [];
+             i=0;
+         }
+     }
+})
+
+console.log(drawings)
