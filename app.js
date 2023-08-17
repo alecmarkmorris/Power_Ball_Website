73ec5b9630b53d946b4ff5f6c9b58bbae6806628
alecmarkmorris <Alecmarkmorris@gmail.com>
2023-08-17 14:06:28 -0700

    Adding functions to JS That take care of API calls

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const POWERBALLAPI_URL = 'https://dark-rose-betta-ring.cyclic.app/2023';
-
+const POWERBALLAPI_URL = 'https://dark-rose-betta-ring.cyclic.app/';
 
 //array to store all drawing arrays
 var drawings = [];
@@ -10,24 +9,55 @@ var dates =[];
 //Var to help count when the next
 //array is in the json 
 var i =0;
-    fetch(POWERBALLAPI_URL)
-        .then(response => response.json())
-        .then(responseJson => {
-            for(let fact of responseJson ){
-                i++;
-                dates.push(fact)
+
+//Var to store the year Selected by user
+var year= "";
+
+
+//Function determine Selected options 
+//Inluding Year,....
+function selected(){
+    year = document.getElementById("years")
+    yearSelected(year.options[year.selectedIndex].value);
+}
+
+
+//Helper function that logs the year Selected
+function yearSelected(year){
+    if(year == "Select Year"){
+        alert("Please Select a Year")
+    }
+    console.log("Selected Year is: " + year)
+    apiCall(year)
+}
+
+
+//Function that calls API
+//And stores all the drawings for a specified range
+function apiCall(year){
+    console.log("The URL is : " + POWERBALLAPI_URL + year)
+    fetch(POWERBALLAPI_URL + year)
+    .then(response => response.json())
+    .then(responseJson => {
+        
+        for(let fact of responseJson ){
+            i++;
+            dates.push(fact)
 
             if(i == 7){
-             //console.log(dates)
-             drawings.push(dates)
-             dates = [];
+                //console.log(dates)
+                drawings.push(dates)
+                dates = [];
              i=0;
-         }
-     }
-})
-
-function myFunction(){
-    console.log(drawings)
-    document.getElementById("all").innerText
+            }
+        }
+    })
+ console.log(drawings)
 }
-//console.log(drawings)
+
+
+//Must clear array before testing a new year
+drawings = [];//Must clear array before testing a new year
+drawings = [];//Must clear array before testing a new year
+drawings = [];//Must clear array before testing a new year
+drawings = [];
